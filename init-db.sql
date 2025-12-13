10eb7584a4e91b016d3616bf7d0d42dfae50e782
Kevin Truong <Kevin.truong@logicsolutions.com>
2025-12-13 17:36:40 -0500

    fix: use IF NOT EXISTS for PostgreSQL database creation

--- init-db.sql
+++ init-db.sql
@@ -1,33 +1,7 @@
--- Create rbac_db if it doesn't exist
-DO
-$$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'rbac_db') THEN
-      PERFORM pg_sleep(1); -- small delay avoids race conditions
-      EXECUTE 'CREATE DATABASE rbac_db';
-   END IF;
-END
-$$;
-
--- Create booking_db if it doesn't exist
-DO
-$$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'booking_db') THEN
-      EXECUTE 'CREATE DATABASE booking_db';
-   END IF;
-END
-$$;
-
--- Create notification_db if it doesn't exist
-DO
-$$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'notification_db') THEN
-      EXECUTE 'CREATE DATABASE notification_db';
-   END IF;
-END
-$$;
+-- Create databases (executed directly, not in a function)
+CREATE DATABASE IF NOT EXISTS rbac_db;
+CREATE DATABASE IF NOT EXISTS booking_db;
+CREATE DATABASE IF NOT EXISTS notification_db;
 
 -- Grant permissions
 GRANT ALL PRIVILEGES ON DATABASE rbac_db TO postgres;
